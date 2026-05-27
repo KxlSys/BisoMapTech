95179631e6dfe876d9a5b159fd2ec41338cd767e
Kalel <116387953+KalelDAMBA@users.noreply.github.com>
2026-05-27 23:54:21 +0200

    chore: remove sentry test button

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import "./index.css"
-import App from "./App.tsx"
-import { ThemeProvider } from "@/components/theme-provider.tsx"
+import "./index.css";
+import App from "./App.tsx";
+import { ThemeProvider } from "@/components/theme-provider.tsx";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://adf56d9b9b8e9d362fd875cf293217da@o4511463914340352.ingest.de.sentry.io/4511463936229456",
+  sendDefaultPii: true,
+  tracesSampleRate: 1.0,
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -11,4 +18,4 @@ createRoot(document.getElementById("root")!).render(
       <App />
     </ThemeProvider>
   </StrictMode>
-)
+);
